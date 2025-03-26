8bd8f2c590c15beb4cb80cf66a15a749350d267e
dy0917 <huangkingsley@gmail.com>
2025-03-26 22:05:38 +1300

    fix type issue

--- expressTypescript/src/index.ts
+++ expressTypescript/src/index.ts
@@ -2,23 +2,28 @@ import express, { Request, Response, NextFunction, response } from "express";
 import postRouter from "./routes/post";
 import loginRouter from "./routes/login";
 import todoRouter from "./routes/todo";
-import cors from 'cors';
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/", express.static("public"));
 app.use("/post", postRouter);
 app.use("/todos", todoRouter);
 app.use("/login", loginRouter);
 
+interface QueryTypes {
+  num1?: string;
+  num2?: string;
+}
+
 app.get("/add", (req: Request, res: Response) => {
   const query = req.query;
-  const { num1, num2 } = query;
+  const { num1, num2 }: QueryTypes = query;
   console.log(query);
-  // const result = parseInt(num1) + parseInt(num2);
-  res.send({ num1, num2 });
+  const result = parseInt(num1!) + parseInt(num2!);
+  res.send({result});
 });
 
 app.listen(port, () => {
