d608c0b0a48508ac72462c97c041c44a5e2c3872
dy0917 <huangkingsley@gmail.com>
2025-04-02 21:06:33 +1300

    reverse changes

--- loginDemoJs/src/routes/login.js
+++ loginDemoJs/src/routes/login.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { Login } = require("../controllers/login");
-const login = new Login();
+const { createANewAccount, login } = require("../controllers/login");
 
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
-  login.createANewAccount(username, password);
+  createANewAccount(username, password);
   res.sendStatus(204);
 });
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  const result = await login.login(username, password);
+  const result = await login(username, password);
   console.log("result", result);
   result ? res.send({ token: result }) : res.sendStatus(403);
 });
